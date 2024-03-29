ddf9bf65682539e8d86a207ed0bb3b65bef6de54
Cerenar <camerondonner63@gmail.com>
2024-03-29 10:07:56 -0500

    Set up project files

---
+++ script.js
@@ -0,0 +1,16 @@
+const library = [];
+
+function Book (title, author, pageCount, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+
+}
+
+for (let book in library) {
+    
+}
