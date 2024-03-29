4fd162a24753d73e113c3b575f38e5449255648b
Cerenar <camerondonner63@gmail.com>
2024-03-29 11:43:33 -0500

    Get books loading on to page from library array

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const library = [];
+let book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Have read');
+let book2 = new Book('c', 'd', 2, 'no');
+
+const library = [book1, book2];
+const container = document.querySelector('.container');
 
 function Book (title, author, pageCount, readStatus) {
     this.title = title;
@@ -8,9 +12,36 @@ function Book (title, author, pageCount, readStatus) {
 }
 
 function addBookToLibrary() {
-
+    let title = prompt('title');
+    let author = prompt('author');
+    let pageCount = prompt('pageCount');
+    let readStatus = prompt('read');
+    let newBook = new Book(title, author, pageCount, readStatus);
+    library.push(newBook);
 }
 
-for (let book in library) {
-    
+function addBookToWebpage(library) {
+    for (let i = library.length-1; i >= 0; i--) {
+        let newBook = document.querySelector(".container").appendChild(
+            elt("div",
+                elt("h2", `${library[i].title}`),
+                elt("p", `${library[i].author}`),
+                elt("p", `${library[i].pageCount}`),
+                elt("p", `${library[i].readStatus}`)));
+        newBook.classList.add('book');
+        container.append(newBook);
+        library.pop(library[i]);
+    }
+}
+
+addBookToWebpage(library);
+
+
+function elt(type, ...children) {
+  let node = document.createElement(type);
+  for (let child of children) {
+    if (typeof child != "string") node.appendChild(child);
+    else node.appendChild(document.createTextNode(child));
+  }
+  return node;
 }
